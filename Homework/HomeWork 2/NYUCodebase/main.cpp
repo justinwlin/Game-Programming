22efacd4f3b39b5227bba1ae55fbacd8e1af5a41
Justin Lin <justinwlin@gmail.com>
2018-10-07 22:17:26 -0400

    Cleaning up code and separating into proper game logic

---
+++ Homework/HomeWork 2/NYUCodebase/main.cpp
@@ -0,0 +1,95 @@
+#ifdef _WINDOWS
+#include <GL/glew.h>
+#endif
+#include <SDL.h>
+#include <SDL_opengl.h>
+#include <SDL_image.h>
+#include "ShaderProgram.h"
+#include "glm/mat4x4.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#ifdef _WINDOWS
+#define RESOURCE_FOLDER ""
+#else
+#define RESOURCE_FOLDER "NYUCodebase.app/Contents/Resources/"
+#endif
+
+SDL_Window* displayWindow;
+ShaderProgram program;
+SDL_Event event;
+
+const float width = 640;
+const float height = 360;
+bool loop = false;
+
+glm::mat4 projectionMatrix;
+glm::mat4 modelMatrix;
+glm::mat4 viewMatrix;
+
+void Startup(){
+    SDL_Init(SDL_INIT_VIDEO);
+    displayWindow = SDL_CreateWindow("My Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_OPENGL);
+    SDL_GLContext context = SDL_GL_CreateContext(displayWindow);
+    SDL_GL_MakeCurrent(displayWindow, context);
+    glViewport(0, 0, width, height);
+
+    program.Load(RESOURCE_FOLDER "vertex.glsl", RESOURCE_FOLDER "fragment.glsl");
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glUseProgram(program.programID);
+    
+    projectionMatrix = glm::mat4(1.0f);
+    modelMatrix = glm::mat4(1.0f);
+    viewMatrix = glm::mat4(1.0f);
+    projectionMatrix = glm::ortho(-1.777f, 1.777f, -1.0f, 1.0f, -1.0f, 1.0f);
+    
+    #ifdef _WINDOWS
+        glewInit();
+    #endif
+    
+}
+
+void ProcessEvents(){
+    while (SDL_PollEvent(&event)) {
+        if (event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE) {
+            loop = true;
+        }
+    }
+}
+
+void Update(){
+    program.SetModelMatrix(modelMatrix);
+    program.SetProjectionMatrix(projectionMatrix);
+    program.SetViewMatrix(viewMatrix);
+    
+    //Square
+    float vertices[] = {-0.5, -0.5, 0.5, -0.5, 0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5, 0.5};
+    //First object
+    glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices);
+    glEnableVertexAttribArray(program.positionAttribute);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+}
+
+void Renderer(){
+    float angle = 10.0f * (3.1415926f / 180.0f);
+    modelMatrix = glm::rotate(modelMatrix, angle, glm::vec3(0.0f, 0.0f, 1.0f));
+
+    SDL_GL_SwapWindow(displayWindow);
+    glClear(GL_COLOR_BUFFER_BIT);
+}
+
+int main(int argc, char *argv[])
+{
+    Startup();
+    while (!loop) {
+        ProcessEvents();
+        Update();
+        Renderer();
+    }
+    
+    SDL_Quit();
+    return 0;
+}
